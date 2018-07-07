aa23a1b60bb142a7cd992b756dc481fc406bd4f1
kat <kanemoto.k@mccd.edu>
2018-07-06 18:52:54 -0700

    This is the commit that goes along with the instructions on taking a 'Hello World' app and transforming it into a start screen with a backspalash, 2 nonpushable buttons, and a dancing gif.

--- app/src/main/java/com/example/kathykanemoto/anndroidinalgorithmland_v2/MainActivity.java
+++ app/src/main/java/com/example/kathykanemoto/anndroidinalgorithmland_v2/MainActivity.java
@@ -9,5 +9,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //for the character gif
+        GifView pGif = (GifView) findViewById(R.id.viewGif);
+        pGif.setImageResource(R.drawable.anndroid);
     }
 }
