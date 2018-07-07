258cb39fe90a0827fd7832a8fe3113752158a343
kat <kanemoto.k@mccd.edu>
2018-07-06 19:35:20 -0700

    added the sound for the theme song

--- app/src/main/java/com/example/kathykanemoto/anndroidinalgorithmland_v2/MainActivity.java
+++ app/src/main/java/com/example/kathykanemoto/anndroidinalgorithmland_v2/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.kathykanemoto.anndroidinalgorithmland_v2;
 
+import android.content.pm.ActivityInfo;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -10,8 +12,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //setting the orientation to landscape
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
         //for the character gif
         GifView pGif = (GifView) findViewById(R.id.viewGif);
         pGif.setImageResource(R.drawable.anndroid);
+
+        //to play the theme song
+        MediaPlayer themeSong = MediaPlayer.create(getApplicationContext(), R.raw.themesong);
+        themeSong.start();
     }
 }
